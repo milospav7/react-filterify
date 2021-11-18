6ed1ea26171986ef7d82ead8feb006f14c47fe2f
Milos Pavlovic <milosar7@gmail.com>
2021-11-18 21:44:51 +0100

    Button group filter unit tests

---
+++ src/test/ButtonGroupFilter.test.js
@@ -0,0 +1,39 @@
+import { screen } from "@testing-library/react";
+import ButtonGroupFilter from "../components/ButtonGroupFilter";
+import { render } from "../test-utils";
+
+test("Button group filter with label", () => {
+  render(
+    <ButtonGroupFilter
+      containerId="F2_Test"
+      withAssociatedLabel
+      label="Region"
+      isMulti
+      filteringProperty="Region"
+      options={["Šumadija", "Banat", "Srem"]}
+      className="text-left mb-3"
+    />
+  );
+  expect(screen.getByText(/region/i)).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /šumadija/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /banat/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /srem/i })).toBeInTheDocument();
+});
+
+test("Button group filter without label", () => {
+  render(
+    <ButtonGroupFilter
+      containerId="F2_Test"
+      isMulti
+      filteringProperty="Region"
+      options={["Šumadija", "Banat", "Srem"]}
+      className="text-left mb-3"
+    />
+  );
+  expect(screen.queryByText(/region/i)).not.toBeInTheDocument(); // ** getByText will throw error if nothing found
+  expect(screen.getByRole("button", { name: /šumadija/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /banat/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /srem/i })).toBeInTheDocument();
+});
+
+// To throw error without container id
