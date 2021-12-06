ea273780483c1f10f9718d4d2e7362795dcf1c7a
Milos Pavlovic <milosar7@gmail.com>
2021-12-06 19:10:32 +0100

    Make additional operators optional for TextFilter

--- src/components/TextFilter.tsx
+++ src/components/TextFilter.tsx
@@ -11,15 +11,7 @@ import {
   InputGroupText,
 } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAsterisk,
-  faCaretDown,
-  faCommentDots,
-  faCommentSlash,
-  faEquals,
-  faNotEqual,
-} from "@fortawesome/free-solid-svg-icons";
-import { ValueTypedObject } from "../store/types";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import {
   useFilterActions,
   useFilterState,
@@ -29,47 +21,18 @@ import {
 import { DebouncedInputField } from "./DebouncedInputField";
 import { BaseFilterProps } from "../store/interfaces";
 import FilterDecorator from "./FilterDecorator";
+import {
+  faIconByOperator,
+  generateCustomExpression,
+  operatorSymbols,
+} from "./TextFilter.utils";
+import RenderIf from "./RenderIf";
 
-const operatorSymbols: ValueTypedObject<string> = {
-  contains: "contains",
-  doesnotcontain: "doesnotcontain",
-  startswith: "startswith",
-  endswith: "endswith",
-  eq: "eq",
-  ne: "ne",
-};
+interface IProps extends BaseFilterProps {
+  multipleOperators?: boolean;
+}
 
-const faIconByOperator: ValueTypedObject<any> = {
-  contains: faCommentDots,
-  doesnotcontain: faCommentSlash,
-  notlike: faCommentSlash,
-  eq: faEquals,
-  ne: faNotEqual,
-  startswith: faAsterisk,
-  endswith: faAsterisk,
-};
-
-//** TODO: should be moved in filter generator part and handled by centralized mechanism. Filter component should ony dispatch type of filter, it's value and selected operator, no need to determine any custom expression from ui components  */
-const generateCustomExpression = (
-  filteringProperty: string,
-  opr: string,
-  value: string
-) => {
-  let generatedExpression = "";
-
-  if (opr === "eq" || opr === "ne")
-    generatedExpression = `(s/${filteringProperty} ${opr} '${value}')`;
-  else if (opr === "contains")
-    generatedExpression = `contains(s/${filteringProperty}, '${value}')`;
-  else if (opr === "doesnotcontain")
-    generatedExpression = `(indexof(s/${filteringProperty}, '${value}') eq -1)`;
-  else if (opr === "startswith" || opr === "endswith")
-    generatedExpression = `${opr}(s/${filteringProperty}, '${value}')`;
-
-  return generatedExpression;
-};
-
-const TextFilter: React.FC<BaseFilterProps> = ({
+const TextFilter: React.FC<IProps> = ({
   containerId,
   filteringProperty,
   navigationProperty,
@@ -79,6 +42,7 @@ const TextFilter: React.FC<BaseFilterProps> = ({
   label,
   style,
   placeholder,
+  multipleOperators = true,
 }) => {
   const { propertyFilters, navigationPropertyFilters } =
     useContainerState(containerId);
@@ -105,12 +69,13 @@ const TextFilter: React.FC<BaseFilterProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [operatorsDropdownOpen, setOperatorsoperatorsDropdownOpen] =
+    useState(false);
   const [operator, setOperator] = useState(getInitialOperator());
   const inputRef = useRef<Input>(null);
   const toggleDropdown = useCallback(
-    () => setDropdownOpen(!dropdownOpen),
-    [dropdownOpen]
+    () => setOperatorsoperatorsDropdownOpen(!operatorsDropdownOpen),
+    [operatorsDropdownOpen]
   );
 
   const { updateFilter } = useFilterActions(
@@ -154,6 +119,91 @@ const TextFilter: React.FC<BaseFilterProps> = ({
     [operator]
   );
 
+  const filterOperators = useMemo(
+    () => (
+      <RenderIf condition={!!multipleOperators}>
+        <InputGroupAddon addonType="prepend">
+          <ButtonDropdown
+            isOpen={operatorsDropdownOpen}
+            toggle={toggleDropdown}
+          >
+            <DropdownToggle
+              data-testid={`${containerId}-oprs-menu-btn`}
+              className="p-0 m-0 rounded-left text-muted z-index-auto"
+            >
+              <FontAwesomeIcon icon={faCaretDown} className="mx-1 text-light" />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.contains)}
+                onClick={() => updateOperator(operatorSymbols.contains)}
+              >
+                Contains
+              </DropdownItem>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.doesnotcontain)}
+                onClick={() => updateOperator(operatorSymbols.doesnotcontain)}
+              >
+                Does not contain
+              </DropdownItem>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.startswith)}
+                onClick={() => updateOperator(operatorSymbols.startswith)}
+              >
+                Starts with
+              </DropdownItem>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.endswith)}
+                onClick={() => updateOperator(operatorSymbols.endswith)}
+              >
+                Ends with
+              </DropdownItem>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.eq)}
+                onClick={() => updateOperator(operatorSymbols.eq)}
+              >
+                Equal
+              </DropdownItem>
+              <DropdownItem
+                active={operatorSelected(operatorSymbols.ne)}
+                onClick={() => updateOperator(operatorSymbols.ne)}
+              >
+                Not equal
+              </DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </InputGroupAddon>
+        <InputGroupAddon addonType="prepend">
+          <InputGroupText className="text-muted">
+            {operator === operatorSymbols.endswith && (
+              <span style={{ lineHeight: "1" }} className="mr-1">
+                ....
+              </span>
+            )}
+            <FontAwesomeIcon
+              icon={faIconByOperator[operator]}
+              style={{ fontSize: ".9em" }}
+            />
+            {operator === operatorSymbols.startswith && (
+              <span style={{ lineHeight: "1" }} className="ml-1">
+                ....
+              </span>
+            )}
+          </InputGroupText>
+        </InputGroupAddon>
+      </RenderIf>
+    ),
+    [
+      multipleOperators,
+      operatorsDropdownOpen,
+      toggleDropdown,
+      containerId,
+      operatorSelected,
+      operator,
+      updateOperator,
+    ]
+  );
+
   const memoizedFilter = useMemo(
     () => (
       <FilterDecorator
@@ -166,77 +216,7 @@ const TextFilter: React.FC<BaseFilterProps> = ({
       >
         <>
           <InputGroup size="sm">
-            <InputGroupAddon addonType="prepend">
-              <ButtonDropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
-                <DropdownToggle
-                  data-testid={`${containerId}-oprs-menu-btn`}
-                  className="p-0 m-0 rounded-left text-muted z-index-auto"
-                >
-                  <FontAwesomeIcon
-                    icon={faCaretDown}
-                    className="mx-1 text-light"
-                  />
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.contains)}
-                    onClick={() => updateOperator(operatorSymbols.contains)}
-                  >
-                    Contains
-                  </DropdownItem>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.doesnotcontain)}
-                    onClick={() =>
-                      updateOperator(operatorSymbols.doesnotcontain)
-                    }
-                  >
-                    Does not contain
-                  </DropdownItem>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.startswith)}
-                    onClick={() => updateOperator(operatorSymbols.startswith)}
-                  >
-                    Starts with
-                  </DropdownItem>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.endswith)}
-                    onClick={() => updateOperator(operatorSymbols.endswith)}
-                  >
-                    Ends with
-                  </DropdownItem>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.eq)}
-                    onClick={() => updateOperator(operatorSymbols.eq)}
-                  >
-                    Equal
-                  </DropdownItem>
-                  <DropdownItem
-                    active={operatorSelected(operatorSymbols.ne)}
-                    onClick={() => updateOperator(operatorSymbols.ne)}
-                  >
-                    Not equal
-                  </DropdownItem>
-                </DropdownMenu>
-              </ButtonDropdown>
-            </InputGroupAddon>
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText className="text-muted">
-                {operator === operatorSymbols.endswith && (
-                  <span style={{ lineHeight: "1" }} className="mr-1">
-                    ....
-                  </span>
-                )}
-                <FontAwesomeIcon
-                  icon={faIconByOperator[operator]}
-                  style={{ fontSize: ".9em" }}
-                />
-                {operator === operatorSymbols.startswith && (
-                  <span style={{ lineHeight: "1" }} className="ml-1">
-                    ....
-                  </span>
-                )}
-              </InputGroupText>
-            </InputGroupAddon>
+            {filterOperators}
             <DebouncedInputField
               inputRef={inputRef}
               filteringProperty={filteringProperty}
@@ -249,8 +229,20 @@ const TextFilter: React.FC<BaseFilterProps> = ({
         </>
       </FilterDecorator>
     ),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [filterValue, dropdownOpen]
+    [
+      withAssociatedLabel,
+      className,
+      labelClassName,
+      label,
+      style,
+      styles.label,
+      styles.input,
+      filterOperators,
+      filteringProperty,
+      filterValue,
+      updateTargetFilter,
+      placeholder,
+    ]
   );
 
   return memoizedFilter;
---
+++ src/components/TextFilter.utils.ts
@@ -0,0 +1,49 @@
+import {
+  faAsterisk,
+  faCommentDots,
+  faCommentSlash,
+  faEquals,
+  faNotEqual,
+} from "@fortawesome/free-solid-svg-icons";
+import { ValueTypedObject } from "../store/types";
+
+const operatorSymbols: ValueTypedObject<string> = {
+  contains: "contains",
+  doesnotcontain: "doesnotcontain",
+  startswith: "startswith",
+  endswith: "endswith",
+  eq: "eq",
+  ne: "ne",
+};
+
+const faIconByOperator: ValueTypedObject<any> = {
+  contains: faCommentDots,
+  doesnotcontain: faCommentSlash,
+  notlike: faCommentSlash,
+  eq: faEquals,
+  ne: faNotEqual,
+  startswith: faAsterisk,
+  endswith: faAsterisk,
+};
+
+//** TODO: should be moved in filter generator part and handled by centralized mechanism. Filter component should ony dispatch type of filter, it's value and selected operator, no need to determine any custom expression from ui components  */
+const generateCustomExpression = (
+  filteringProperty: string,
+  opr: string,
+  value: string
+) => {
+  let generatedExpression = "";
+
+  if (opr === "eq" || opr === "ne")
+    generatedExpression = `(s/${filteringProperty} ${opr} '${value}')`;
+  else if (opr === "contains")
+    generatedExpression = `contains(s/${filteringProperty}, '${value}')`;
+  else if (opr === "doesnotcontain")
+    generatedExpression = `(indexof(s/${filteringProperty}, '${value}') eq -1)`;
+  else if (opr === "startswith" || opr === "endswith")
+    generatedExpression = `${opr}(s/${filteringProperty}, '${value}')`;
+
+  return generatedExpression;
+};
+
+export { operatorSymbols, faIconByOperator, generateCustomExpression };
